cda1f6c07ddbaf765f0dbb95f9eb8942d553899d
Merlinz <jamkapp556@gmail.com>
2023-04-06 08:10:29 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import './index.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './Redux/store'
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
